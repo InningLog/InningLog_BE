c480ffc9afa5d544e6eb7b97953dd167f3818b68
GOOHAESEUNG <amu6675@naver.com>
2025-05-19 22:35:45 +0900

    초기 설정

---
+++ src/main/java/com/inninglog/inninglog/InninglogApplication.java
@@ -0,0 +1,13 @@
+package com.inninglog.inninglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InninglogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InninglogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inninglog/inninglog/InninglogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inninglog.inninglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InninglogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
