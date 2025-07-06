3c3625ff15041d236e93ddc11801d773c8390428
GOOHAESEUNG <amu6675@naver.com>
2025-07-06 17:36:11 +0900

    feat : 좌석 해시태그 수정 #34

--- src/main/java/com/inninglog/inninglog/seatView/SeatViewEmotionTagInitializer.java
+++ src/main/java/com/inninglog/inninglog/seatView/SeatViewEmotionTagInitializer.java
@@ -18,12 +18,12 @@ public class SeatViewEmotionTagInitializer implements CommandLineRunner {
     public void run(String... args) {
         if (tagRepository.count() == 0) {
             tagRepository.saveAll(List.of(
-                    SeatViewEmotionTag.builder().code("VIEW_OPEN").label("시야_탁_트였어요").build(),
-                    SeatViewEmotionTag.builder().code("SUN_STRONG").label("햇빛이_강해서_모자는_필수").build(),
+                    SeatViewEmotionTag.builder().code("VIEW_OPEN").label("시야_탁_트임").build(),
+                    SeatViewEmotionTag.builder().code("SUN_STRONG").label("햇빛이_강함").build(),
                     SeatViewEmotionTag.builder().code("CHEERING_BEST").label("응원_분위기_최고").build(),
-                    SeatViewEmotionTag.builder().code("CHEER_STAGE_NEAR").label("응원단상이_가까워요").build(),
-                    SeatViewEmotionTag.builder().code("GOOD_VALUE").label("가성비_좌석인듯").build(),
-                    SeatViewEmotionTag.builder().code("ROOF_SHELTER").label("지붕_있어서_비와도_안심").build()
+                    SeatViewEmotionTag.builder().code("CHEER_STAGE_NEAR").label("응원단상이_가까움").build(),
+                    SeatViewEmotionTag.builder().code("GOOD_VALUE").label("가성비_최고").build(),
+                    SeatViewEmotionTag.builder().code("ROOF_SHELTER").label("지붕_있음").build()
             ));
         }
     }
