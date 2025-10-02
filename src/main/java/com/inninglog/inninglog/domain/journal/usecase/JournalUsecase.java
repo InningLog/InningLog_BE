738da5fc93d200b2b8f91189f6bf1bb581edf16d
GOOHAESEUNG <amu6675@naver.com>
2025-10-02 23:04:26 +0900

    feat : 구장 검증 로직 분리 #72

--- src/main/java/com/inninglog/inninglog/domain/journal/usecase/JournalUsecase.java
+++ src/main/java/com/inninglog/inninglog/domain/journal/usecase/JournalUsecase.java
@@ -1,8 +1,12 @@
 package com.inninglog.inninglog.domain.journal.usecase;
 
+import com.inninglog.inninglog.domain.journal.dto.req.JourCreateReqDto;
 import com.inninglog.inninglog.domain.journal.dto.res.JourCreateResDto;
 import com.inninglog.inninglog.domain.journal.service.JournalService;
+import com.inninglog.inninglog.domain.member.domain.Member;
 import com.inninglog.inninglog.domain.member.service.MemberValidateService;
+import com.inninglog.inninglog.domain.team.domain.Team;
+import com.inninglog.inninglog.domain.team.service.TeamvalidateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +17,13 @@ public class JournalUsecase {
 
     private final JournalService journalService;
     private final MemberValidateService memberValidateService;
+    private final TeamvalidateService teamvalidateService;
 
     @Transactional
-    public JourCreateResDto createJournal(Long memberId, JourCreateResDto dto) {
-        memberValidateService.findById(memberId);
+    public JourCreateResDto createJournal(Long memberId, JourCreateReqDto dto) {
+       Member member = memberValidateService.findById(memberId);
+       Team opponentTeam = teamvalidateService.validateTeam(dto.getOpponentTeamSC());
+
 
 
     }
