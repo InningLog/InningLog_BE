abdc0a5d0db5c99189d730f221f7c80a510ec5a2
GOOHAESEUNG <amu6675@naver.com>
2025-11-12 20:31:06 +0900

    feat: 좋아요 엔티티 생성 #76

---
+++ src/main/java/com/inninglog/inninglog/domain/contentType/ContentType.java
@@ -0,0 +1,7 @@
+package com.inninglog.inninglog.domain.contentType;
+
+public enum ContentType {
+    POST,        // 일반 게시글
+    JOURNAL,     // 직관 일지
+    MARKET       // 이닝장터
+}
---
+++ src/main/java/com/inninglog/inninglog/domain/like/domain/Like.java
@@ -0,0 +1,41 @@
+package com.inninglog.inninglog.domain.like.domain;
+
+import com.inninglog.inninglog.domain.contentType.ContentType;
+import com.inninglog.inninglog.domain.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ContentType contentType;
+
+    @Column(nullable = false)
+    private Long targetId;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
