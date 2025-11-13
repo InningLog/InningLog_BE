7906c7c05f6934640e2607d713cfa34416faf2b7
GOOHAESEUNG <amu6675@naver.com>
2025-11-13 18:21:04 +0900

    feat: 게시글 생성 서비스 로직 #77

--- src/main/java/com/inninglog/inninglog/domain/post/service/PostCreateService.java
+++ src/main/java/com/inninglog/inninglog/domain/post/service/PostCreateService.java
@@ -1,5 +1,8 @@
 package com.inninglog.inninglog.domain.post.service;
 
+import com.inninglog.inninglog.domain.member.domain.Member;
+import com.inninglog.inninglog.domain.post.domain.Post;
+import com.inninglog.inninglog.domain.post.dto.req.PostCreateReqDto;
 import com.inninglog.inninglog.domain.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +14,8 @@ public class PostCreateService {
     private final PostRepository postRepository;
 
     //게시글 생성
+    public void createPost(PostCreateReqDto dto,String team_shortcode, Member member){
+        Post post = Post.of(dto, team_shortcode, member);
+        postRepository.save(post);
+    }
 }
