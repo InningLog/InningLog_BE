e7ed4afc17e40bb02cc01abedc0c60ac262fcb30
GOOHAESEUNG <amu6675@naver.com>
2025-11-13 21:54:25 +0900

    feat: 게시글 단건 조회 유스케이스 작성 #78

--- src/main/java/com/inninglog/inninglog/domain/post/service/PostUsecase.java
+++ src/main/java/com/inninglog/inninglog/domain/post/service/PostUsecase.java
@@ -1,8 +1,15 @@
 package com.inninglog.inninglog.domain.post.service;
 
+import com.inninglog.inninglog.domain.contentImage.dto.res.ImageListResDto;
+import com.inninglog.inninglog.domain.contentImage.service.ImageGetService;
 import com.inninglog.inninglog.domain.contentImage.service.PostImageCreateService;
+import com.inninglog.inninglog.domain.contentType.ContentType;
 import com.inninglog.inninglog.domain.member.domain.Member;
+import com.inninglog.inninglog.domain.member.dto.res.MemberShortResDto;
+import com.inninglog.inninglog.domain.member.service.MemberGetService;
+import com.inninglog.inninglog.domain.post.domain.Post;
 import com.inninglog.inninglog.domain.post.dto.req.PostCreateReqDto;
+import com.inninglog.inninglog.domain.post.dto.res.PostSingleResDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +20,23 @@ public class PostUsecase {
 
     private final PostCreateService postCreateService;
     private final PostImageCreateService postImageCreateService;
+    private final PostGetService postGetService;
+    private final PostValidateService postValidateService;
+    private final MemberGetService memberGetService;
+    private final ImageGetService imageGetService;
 
     //게시글 업로드 + 이미지 저장
     @Transactional
     public void uploadPost (PostCreateReqDto dto,String team_Shortcode, Member member){
         Long postId = postCreateService.createPost(dto,team_Shortcode,member);
         postImageCreateService.createPostImageList(postId, dto.imageCreateReqDto());
+    }
+
+    //게시글 단일 조회
+    public PostSingleResDto getSinglePost(ContentType contentType, Long postId, Member member){
+        Post post = postValidateService.getPostById(postId);
+        MemberShortResDto memberShortResDto = memberGetService.toMemberShortResDto(member);
+        ImageListResDto imageListResDto = imageGetService.getImageList(contentType, postId);
+        return postGetService.getSinglePost(post, memberShortResDto, imageListResDto);
     }
 }
