25802b580f472e775d6e6c3f7ad918f493465b65
GOOHAESEUNG <amu6675@naver.com>
2025-11-13 18:37:23 +0900

    feat: 게시글 이미지 업로드 로직 #77

--- src/main/java/com/inninglog/inninglog/domain/contentImage/dto/res/ImageUploadResDto.java
+++ src/main/java/com/inninglog/inninglog/domain/contentImage/dto/res/ImageUploadResDto.java
@@ -1,4 +1,10 @@
 package com.inninglog.inninglog.domain.contentImage.dto.res;
 
-public class ImageUploadResDto {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "이미지 업로드 url DTO")
+public record ImageUploadResDto(
+        Integer sequence,
+        String presignedUrl
+) {
 }
--- src/main/java/com/inninglog/inninglog/domain/contentImage/service/ContentImageCreateService.java
+++ src/main/java/com/inninglog/inninglog/domain/contentImage/service/ContentImageCreateService.java
@@ -1,7 +1,10 @@
 package com.inninglog.inninglog.domain.contentImage.service;
 
 import com.inninglog.inninglog.domain.contentImage.dto.req.ImageUploadReqDto;
+import com.inninglog.inninglog.domain.contentImage.dto.res.ImageUploadResDto;
 import com.inninglog.inninglog.global.s3.PreSginedPutService;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +12,34 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ContentImageCreateService {
 
-    PreSginedPutService preSginedPutService;
+    private final PreSginedPutService preSginedPutService;
 
-    public String putPostImage(ImageUploadReqDto dto, Long memberId){
-        return preSginedPutService.postPutPreUrl(memberId, dto.fileName(), dto.contentType());
+    public List<ImageUploadResDto> getPostImagePresignedUrl(
+            List<ImageUploadReqDto> dtos,
+            Long memberId
+    ) {
+
+        List<ImageUploadResDto> result = new ArrayList<>();
+
+        for (ImageUploadReqDto dto : dtos) {
+            String url = putPostImage(dto, memberId);
+
+            result.add(
+                    new ImageUploadResDto(
+                            dto.sequence(),
+                            url
+                    )
+            );
+        }
+
+        return result;
+    }
+
+    private String putPostImage(ImageUploadReqDto dto, Long memberId) {
+        return preSginedPutService.postPutPreUrl(
+                memberId,
+                dto.fileName(),
+                dto.contentType()
+        );
     }
 }
