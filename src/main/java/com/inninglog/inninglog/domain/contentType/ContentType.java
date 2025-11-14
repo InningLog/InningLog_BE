9b393b5d171f31fe8121638eb84cce5f6ce185c4
GOOHAESEUNG <amu6675@naver.com>
2025-11-14 20:17:19 +0900

    feat : contenttype 댓글 추가 #82

--- src/main/java/com/inninglog/inninglog/domain/contentType/ContentType.java
+++ src/main/java/com/inninglog/inninglog/domain/contentType/ContentType.java
@@ -1,7 +1,8 @@
 package com.inninglog.inninglog.domain.contentType;
 
 public enum ContentType {
-    POST,        // 일반 게시글
+    POST, // 일반 게시글
+    COMMENT, //댓글
     JOURNAL,     // 직관 일지
     MARKET       // 이닝장터
 }
