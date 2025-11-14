7d31daa71a5ca82a61e76396c4aa284c4577cfca
GOOHAESEUNG <amu6675@naver.com>
2025-11-14 18:47:14 +0900

    docs :스웨거 설명 추가 #80

--- src/main/java/com/inninglog/inninglog/domain/like/controller/LikeCreateController.java
+++ src/main/java/com/inninglog/inninglog/domain/like/controller/LikeCreateController.java
@@ -3,7 +3,6 @@ package com.inninglog.inninglog.domain.like.controller;
 import com.inninglog.inninglog.domain.contentType.ContentType;
 import com.inninglog.inninglog.domain.like.service.LikeUsecase;
 import com.inninglog.inninglog.global.auth.CustomUserDetails;
-import com.inninglog.inninglog.global.response.SuccessApiResponses;
 import com.inninglog.inninglog.global.response.SuccessCode;
 import com.inninglog.inninglog.global.response.SuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,8 +15,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,43 +24,42 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/community")
 @Tag(name = "좋아요", description = "좋아요 관련 API")
-public class LikeDeleteController {
+public class LikeCreateController {
 
     private final LikeUsecase likeUsecase;
 
     @Operation(
-            summary = "게시글 좋아요 취소",
+            summary = "게시글 좋아요 생성",
             description = """
-                특정 게시글에 현재 로그인한 사용자가 누른 '좋아요'를 취소합니다.
+            특정 게시글에 현재 로그인한 사용자가 좋아요를 누릅니다.
 
-                - 로그인한 사용자가 해당 게시글에 좋아요를 누른 상태일 때만 삭제됩니다.
-                - 좋아요가 존재하지 않는 상태에서 요청 시 예외 발생
-                - 성공 시 좋아요 수 감소
-                """
+            - 이미 좋아요를 누른 상태에서 다시 요청 시 예외 발생
+            - 성공 시 좋아요 수 증가
+            """
     )
     @ApiResponses({
             @ApiResponse(
                     responseCode = "200",
-                    description = "좋아요 취소 성공",
+                    description = "좋아요 성공",
                     content = @Content(schema = @Schema(implementation = SuccessResponse.class))
             ),
             @ApiResponse(
                     responseCode = "404",
-                    description = "좋아요 기록 또는 게시글을 찾을 수 없음"
+                    description = "게시글 없음 / 사용자 없음 등의 예외"
             ),
             @ApiResponse(
-                    responseCode = "401",
-                    description = "인증되지 않은 사용자"
+                    responseCode = "400",
+                    description = "이미 좋아요를 눌렀을 경우"
             )
     })
-    @DeleteMapping("/posts/{postId}/likes")
-    public ResponseEntity<SuccessResponse<Void>> deleteLikeAtPost(
-            @Parameter(description = "좋아요를 취소할 게시글 ID", example = "12")
+    @PostMapping("/posts/{postId}/likes")
+    public ResponseEntity<SuccessResponse<Void>> createLikeAtPost(
+            @Parameter(description = "좋아요를 누를 게시글 ID", example = "12")
             @PathVariable Long postId,
 
             @AuthenticationPrincipal CustomUserDetails user
     ){
-        likeUsecase.deleteLike(ContentType.POST, postId, user.getMember());
+        likeUsecase.createLike(ContentType.POST, postId, user.getMember());
         return ResponseEntity.ok(SuccessResponse.success(SuccessCode.OK));
     }
 }
